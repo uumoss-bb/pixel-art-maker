29e2db0ce4b43f4486370af7ed49ea0c926219d9
Brodie Balser <balserbrodie@gmail.com>
2017-08-11 15:26:31 -0600

    we can grab colors now wooaaahhh

--- main.js
+++ main.js
@@ -3,11 +3,21 @@ document.addEventListener('DOMContentLoaded', function(){
 
   let aPixel = document.getElementsByClassName('aPixel')
   let pixelBox = document.getElementById('pixelBox')
-  console.log(pixelBox)
+  let colorBox = document.getElementById('colorBox')
 
   for (var i = 1; i <= 3115; i++) {
     let newPixel = document.createElement('div')
     pixelBox.appendChild(newPixel)
     newPixel.setAttribute('class', 'aPixel')
   }
+
+  colorBox.addEventListener('click', function(event){
+    var x = window.getComputedStyle(event.target).backgroundColor;
+
+    console.log(x)
+
+  });
+
+
+
 });
