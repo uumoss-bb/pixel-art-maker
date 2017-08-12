e7dcbf51f136b5619b8c400ba9ab1c712f11a84a
Brodie Balser <balserbrodie@gmail.com>
2017-08-11 22:29:07 -0600

    I made some changes, there are two events now. but I still need to get it to drag and hold

--- main.js
+++ main.js
@@ -9,26 +9,41 @@ document.addEventListener('DOMContentLoaded', function(){
   let divP = document.getElementsByClassName('aPixel')
   let brushColor = '';
 
+    //This creates the pixels
   for (var i = 1; i <= 3115; i++) {
     let newPixel = document.createElement('div')
     pixelBox.appendChild(newPixel)
     newPixel.setAttribute('class', 'aPixel')
   }
-// I need to make two functions for two different events
-  main.addEventListener('mouseover', function(event){
+        //This applys the color
+  pixelBox.addEventListener('mousedown', function(event){
 
-      if(event.target.className === divC[0].className) {
-        brushColor = window.getComputedStyle(event.target).backgroundColor
-      }
-      else if(event.target.className === divP[0].className) {
-        console.log("SET THE PIXEL COLOR: "+brushColor);
-        event.target.style.backgroundColor = brushColor
+    event.target.style.backgroundColor = brushColor
 
+    // pixelBox.addEventListener('mouseup', function(event){
+    //
+    //   event.target.reomoveAttribute("background-color")
+    //
+    // })
+  })
+          //Im tryig to remove the backgroundColor on the even.target
+          //its not working tho.
+          
+  // pixelBox.addEventListener('mouseup', function(event){
+  //   event.target.reomoveAttribute("backgroundColor")
+  // })
+      //This gets the color
+  theColors.addEventListener('click', function(event){
 
-      }
+    brushColor = window.getComputedStyle(event.target).backgroundColor
 
-  });
-
-
-
-});
+  })
+      //this is for if you have one click event
+// if(event.target.className === divC[0].className) {
+//   brushColor = window.getComputedStyle(event.target).backgroundColor
+// }
+// else if(event.target.className === divP[0].className) {
+//   console.log("SET THE PIXEL COLOR: "+brushColor);
+//   event.target.style.backgroundColor = brushColor
+// }
+})
